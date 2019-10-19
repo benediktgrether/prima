3b6c0e0ba0a472d7d3fd5f475cc4e000de67a034
Benedikt Grether <info@benediktgrether.de>
2019-10-19 10:12:04 +0200

    Add First Cube to render view in Fudge from Tuesday

--- L02_FirstFudge/src/main.js
+++ L02_FirstFudge/src/main.js
@@ -1,8 +1,24 @@
 var L02_FirstFudge;
 (function (L02_FirstFudge) {
+    var ƒ = FudgeCore;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         var canvas = document.querySelector("canvas");
-        console.log(canvas);
+        ƒ.RenderManager.initialize();
+        ƒ.Debug.log(canvas);
+        // New Node for Quad
+        var node = new ƒ.Node("Quad");
+        var mesh = new ƒ.MeshQuad();
+        var cmpMesh = new ƒ.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        var mtrSolidWhite = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.WHITE));
+        var cmpMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+        var cmpCamera = new ƒ.ComponentCamera();
+        cmpCamera.pivot.translateZ(2);
+        L02_FirstFudge.viewport = new ƒ.Viewport();
+        L02_FirstFudge.viewport.initialize("Viewport", node, cmpCamera, canvas);
+        ƒ.Debug.log(L02_FirstFudge.viewport);
+        L02_FirstFudge.viewport.draw();
     }
 })(L02_FirstFudge || (L02_FirstFudge = {}));
--- L02_FirstFudge/src/main.ts
+++ L02_FirstFudge/src/main.ts
@@ -1,8 +1,35 @@
 namespace L02_FirstFudge {
+    import ƒ = FudgeCore;
+
+
     window.addEventListener("load", handleLoad);
 
+    // What do the viewport?
+    export let viewport: ƒ.Viewport;
+
     function handleLoad(_event: Event): void {
-        let canvas: HTMLCanvasElement = document.querySelector("canvas");
-        console.log(canvas);
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        ƒ.RenderManager.initialize();
+        ƒ.Debug.log(canvas);
+
+        // New Node for Quad
+        let node: ƒ.Node = new ƒ.Node("Quad");
+
+        let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
+        let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+
+        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.WHITE));
+        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+
+        let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+        cmpCamera.pivot.translateZ(2);
+
+        viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        ƒ.Debug.log(viewport);
+
+        viewport.draw();
     }
 }
