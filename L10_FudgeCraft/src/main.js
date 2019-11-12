a3bb0ed20808bda484d5efd389bb69bd72becd76
Benedikt Grether <info@benediktgrether.de>
2019-11-12 15:51:34 +0100

    Edit FudgeGame

--- L10_FudgeCraft/src/main.js
+++ L10_FudgeCraft/src/main.js
@@ -1,108 +1,92 @@
 ///<reference types="./../../Fudge/FudgeCore.js"/> //Path to FudgeCore
 var L10_FudgeCraft;
 (function (L10_FudgeCraft) {
-    var ƒ = FudgeCore;
+    var fudge = FudgeCore;
     window.addEventListener("load", handleLoad);
-    // What do the viewport?
-    var viewport;
-    var fudgeCraft = new ƒ.Node("FudgeCraft");
-    var canvas;
-    //#region T-Fragment
-    var tFragment = new ƒ.Node("tFragment");
-    var tFragmentBottom = new ƒ.Node("tFragmentBottom");
-    var tFragmentTop = new ƒ.Node("tFragmentTop");
-    var tFragmentLeft = new ƒ.Node("tFragmentLeft");
-    var tFragmentRight = new ƒ.Node("tFragmentRight");
-    //#endregion
-    //#region L-Fragment
-    var lFragment = new ƒ.Node("lFragment");
-    var lFragmentTop = new ƒ.Node("lFragmentTop");
-    var lFragmentMiddle = new ƒ.Node("lFragmentMiddle");
-    var lFragmentBottom = new ƒ.Node("lFragmentBottom");
-    var lFragmentBottomRight = new ƒ.Node("lGragmentBottomRight");
-    //#endregion
-    //#region Z-Fragment
-    var zFragment = new ƒ.Node("zFragment");
-    var zFragmentTopLeft = new ƒ.Node("zFragmentTopLeft");
-    var zFragmentTop = new ƒ.Node("zFragmentTop");
-    var zFragmentMiddle = new ƒ.Node("zFragmentMiddle");
-    var zFragmentBottom = new ƒ.Node("zFragmentBottom");
-    var zFragmentBottomRight = new ƒ.Node("zFragmentBottomRight");
-    //#endregion
+    var meshCube = new fudge.MeshCube;
+    var allFragmentsMatrices = [];
+    initializeBuildingMatrices();
+    var translationValues = [[-1, 1], [0, 1], [1, 1],
+        [-1, 0], [0, 0], [1, 0],
+        [-1, -1], [0, -1], [1, -1]];
+    var cmpCamera = new fudge.ComponentCamera();
     function handleLoad(_event) {
-        canvas = document.querySelector("canvas");
-        ƒ.RenderManager.initialize();
-        ƒ.Debug.log(canvas);
-        var pong = createFudgeCraft();
-        var cmpCamera = new ƒ.ComponentCamera();
+        var canvas = document.querySelector("canvas");
+        fudge.RenderManager.initialize();
+        fudge.Debug.log(canvas);
         cmpCamera.pivot.translateZ(50);
-        viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", pong, cmpCamera, canvas);
-        ƒ.Debug.log(viewport);
-        viewport.draw();
-        // FUDGE Core Game Loop and Starting the Loop
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        ƒ.Loop.start();
+        var game = createGame();
+        L10_FudgeCraft.viewport = new fudge.Viewport();
+        L10_FudgeCraft.viewport.initialize("Viewport", game, cmpCamera, canvas);
+        fudge.Debug.log(L10_FudgeCraft.viewport);
+        L10_FudgeCraft.viewport.draw();
+        fudge.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        fudge.Loop.start();
     }
     function update(_event) {
-        // ƒ.Debug.log(keysPressed);
-        ƒ.RenderManager.update();
-        viewport.draw();
+        //cmpCamera.pivot.translateX(0.1);
+        //cmpCamera.pivot.rotateY(0.05);
+        fudge.RenderManager.update();
+        L10_FudgeCraft.viewport.draw();
     }
-    function createFudgeCraft() {
-        var meshCube = new ƒ.MeshCube();
-        var mtrSolidWhite = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(1, 1, 1, 1)));
-        fudgeCraft.appendChild(createNode("WallLeft", meshCube, mtrSolidWhite, new ƒ.Vector2(-22, 0), new ƒ.Vector2(1, 30)));
-        fudgeCraft.appendChild(createNode("WallRight", meshCube, mtrSolidWhite, new ƒ.Vector2(22, 0), new ƒ.Vector2(1, 30)));
-        fudgeCraft.appendChild(createNode("WallTop", meshCube, mtrSolidWhite, new ƒ.Vector2(0, 15), new ƒ.Vector2(45, 1)));
-        fudgeCraft.appendChild(createNode("WallBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(0, -15), new ƒ.Vector2(45, 1)));
-        fudgeCraft.appendChild(tFragment);
-        createTFragment(meshCube, mtrSolidWhite, tFragment);
-        fudgeCraft.appendChild(lFragment);
-        createLFragment(meshCube, mtrSolidWhite, lFragment);
-        fudgeCraft.appendChild(zFragment);
-        createZFragment(meshCube, mtrSolidWhite, lFragment);
-        return fudgeCraft;
+    function createGame() {
+        var game = new fudge.Node("Game");
+        buildFragments(game);
+        return game;
     }
-    function createTFragment(meshCube, mtrSolidWhite, tFragment) {
-        tFragmentBottom = createNode("TFragmentBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(1, 1), new ƒ.Vector2(1, 1));
-        tFragmentTop = createNode("TFragmentTop", meshCube, mtrSolidWhite, new ƒ.Vector2(1, 2), new ƒ.Vector2(1, 1));
-        tFragmentLeft = createNode("TFragmentLeft", meshCube, mtrSolidWhite, new ƒ.Vector2(0, 2), new ƒ.Vector2(1, 1));
-        tFragmentRight = createNode("TFragmentRight", meshCube, mtrSolidWhite, new ƒ.Vector2(2, 2), new ƒ.Vector2(1, 1));
-        tFragment.appendChild(tFragmentBottom);
-        tFragment.appendChild(tFragmentTop);
-        tFragment.appendChild(tFragmentLeft);
-        tFragment.appendChild(tFragmentRight);
+    function buildFragments(_game) {
+        var translationTemp = -20;
+        for (var i = 0; i < allFragmentsMatrices.length; i++) {
+            var baseBlock = new fudge.Node("Base_Block_Fragment");
+            var mtrSoliColor = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 0, 0, 0)));
+            baseBlock.addComponent(new fudge.ComponentMesh(meshCube));
+            baseBlock.addComponent(new fudge.ComponentMaterial(mtrSoliColor));
+            baseBlock.addComponent(new fudge.ComponentTransform);
+            baseBlock.cmpTransform.local.translateX(translationTemp);
+            var buildingMtrx = allFragmentsMatrices[i];
+            for (var j = 0; j < buildingMtrx.length; j++) {
+                if (buildingMtrx[j] == true) {
+                    var subBlock = new fudge.Node("subBlock");
+                    subBlock.addComponent(new fudge.ComponentMesh(meshCube));
+                    subBlock.addComponent(new fudge.ComponentMaterial(mtrSoliColor));
+                    subBlock.addComponent(new fudge.ComponentTransform);
+                    subBlock.cmpTransform.local.translateX(translationValues[j][0]);
+                    subBlock.cmpTransform.local.translateY(translationValues[j][1]);
+                    baseBlock.appendChild(subBlock);
+                    _game.appendChild(baseBlock);
+                }
+            }
+            translationTemp = translationTemp + 5;
+        }
     }
-    function createLFragment(meshCube, mtrSolidWhite, lFragment) {
-        lFragmentTop = createNode("lFragmentTop", meshCube, mtrSolidWhite, new ƒ.Vector2(10, 0), new ƒ.Vector2(1, 1));
-        lFragmentMiddle = createNode("lFragmentMiddle", meshCube, mtrSolidWhite, new ƒ.Vector2(10, -1), new ƒ.Vector2(1, 1));
-        lFragmentBottom = createNode("lFragmentBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(10, -2), new ƒ.Vector2(1, 1));
-        lFragmentBottomRight = createNode("lFragmentBottomRight", meshCube, mtrSolidWhite, new ƒ.Vector2(11, -2), new ƒ.Vector2(1, 1));
-        lFragment.appendChild(lFragmentTop);
-        lFragment.appendChild(lFragmentMiddle);
-        lFragment.appendChild(lFragmentBottom);
-        lFragment.appendChild(lFragmentBottomRight);
-    }
-    function createZFragment(meshCube, mtrSolidWhite, zFragment) {
-        zFragmentTopLeft = createNode("zFragmentTopLeft", meshCube, mtrSolidWhite, new ƒ.Vector2(-11, 1), new ƒ.Vector2(1, 1));
-        zFragmentTop = createNode("zFragmentTop", meshCube, mtrSolidWhite, new ƒ.Vector2(-10, 1), new ƒ.Vector2(1, 1));
-        zFragmentMiddle = createNode("zFragmentMiddle", meshCube, mtrSolidWhite, new ƒ.Vector2(-10, 0), new ƒ.Vector2(1, 1));
-        zFragmentBottom = createNode("zFragmentBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(-10, -1), new ƒ.Vector2(1, 1));
-        zFragmentBottomRight = createNode("zFragmentBottomRight", meshCube, mtrSolidWhite, new ƒ.Vector2(-9, -1), new ƒ.Vector2(1, 1));
-        zFragment.appendChild(zFragmentTopLeft);
-        zFragment.appendChild(zFragmentTop);
-        zFragment.appendChild(zFragmentMiddle);
-        zFragment.appendChild(zFragmentBottom);
-        zFragment.appendChild(zFragmentBottomRight);
-    }
-    function createNode(_name, _mesh, _material, _translation, _scaling) {
-        var node = new ƒ.Node(_name);
-        node.addComponent(new ƒ.ComponentTransform);
-        node.addComponent(new ƒ.ComponentMaterial(_material));
-        node.addComponent(new ƒ.ComponentMesh(_mesh));
-        node.cmpTransform.local.translate(_translation.toVector3());
-        node.getComponent(ƒ.ComponentMesh).pivot.scale(_scaling.toVector3());
-        return node;
+    function initializeBuildingMatrices() {
+        var buildingMtrxIBlock = [false, true, false,
+            false, false, false,
+            false, true, false];
+        allFragmentsMatrices.push(buildingMtrxIBlock);
+        var buildingMtrx2x2Block = [false, false, false,
+            false, false, true,
+            false, true, true];
+        allFragmentsMatrices.push(buildingMtrx2x2Block);
+        var buildingMtrxTBlock = [true, true, true,
+            false, false, false,
+            false, true, false];
+        allFragmentsMatrices.push(buildingMtrxTBlock);
+        var buildingMtrxLBlock = [false, true, false,
+            false, false, false,
+            false, true, true];
+        allFragmentsMatrices.push(buildingMtrxLBlock);
+        var buildingMtrxReversedLBlock = [false, true, false,
+            false, false, false,
+            true, true, false];
+        allFragmentsMatrices.push(buildingMtrxReversedLBlock);
+        var buildingMtrxZBlock = [true, true, false,
+            false, false, false,
+            false, true, true];
+        allFragmentsMatrices.push(buildingMtrxZBlock);
+        var buildingMtrxReversedZBlock = [false, true, true,
+            false, false, false,
+            true, true, false];
+        allFragmentsMatrices.push(buildingMtrxReversedZBlock);
     }
 })(L10_FudgeCraft || (L10_FudgeCraft = {}));
--- L10_FudgeCraft/src/main.ts
+++ L10_FudgeCraft/src/main.ts
@@ -1,148 +1,131 @@
 ///<reference types="./../../Fudge/FudgeCore.js"/> //Path to FudgeCore
 namespace L10_FudgeCraft {
 
-
-    import ƒ = FudgeCore;
+    import fudge = FudgeCore;
 
     window.addEventListener("load", handleLoad);
 
-    // What do the viewport?
-    let viewport: ƒ.Viewport;
+    export let viewport: fudge.Viewport;
 
-    let fudgeCraft: ƒ.Node = new ƒ.Node("FudgeCraft");
+    let meshCube: fudge.MeshCube = new fudge.MeshCube;
+    let allFragmentsMatrices: boolean[][] = [];
 
-    let canvas: HTMLCanvasElement;
+    initializeBuildingMatrices();
 
-    //#region T-Fragment
-    let tFragment: ƒ.Node = new ƒ.Node("tFragment");
-    let tFragmentBottom: ƒ.Node = new ƒ.Node("tFragmentBottom");
-    let tFragmentTop: ƒ.Node = new ƒ.Node("tFragmentTop");
-    let tFragmentLeft: ƒ.Node = new ƒ.Node("tFragmentLeft");
-    let tFragmentRight: ƒ.Node = new ƒ.Node("tFragmentRight");
-    //#endregion
+    let translationValues: number[][] = [[-1, 1], [0, 1], [1, 1],
+                                        [-1, 0], [0, 0], [1, 0],
+                                        [-1, -1], [0, -1], [1, -1]];
 
-    //#region L-Fragment
-    let lFragment: ƒ.Node = new ƒ.Node("lFragment");
-    let lFragmentTop: ƒ.Node = new ƒ.Node("lFragmentTop");
-    let lFragmentMiddle: ƒ.Node = new ƒ.Node("lFragmentMiddle");
-    let lFragmentBottom: ƒ.Node = new ƒ.Node("lFragmentBottom");
-    let lFragmentBottomRight: ƒ.Node = new ƒ.Node("lGragmentBottomRight");
-    //#endregion
-
-    //#region Z-Fragment
-    let zFragment: ƒ.Node = new ƒ.Node("zFragment");
-    let zFragmentTopLeft: ƒ.Node = new ƒ.Node("zFragmentTopLeft");
-    let zFragmentTop: ƒ.Node = new ƒ.Node("zFragmentTop");
-    let zFragmentMiddle: ƒ.Node = new ƒ.Node("zFragmentMiddle");
-    let zFragmentBottom: ƒ.Node = new ƒ.Node("zFragmentBottom");
-    let zFragmentBottomRight: ƒ.Node = new ƒ.Node("zFragmentBottomRight");
-
-    //#endregion
-
+    let cmpCamera: fudge.ComponentCamera = new fudge.ComponentCamera();
+ 
     function handleLoad(_event: Event): void {
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        fudge.RenderManager.initialize();
+        fudge.Debug.log(canvas);
         
-        canvas = document.querySelector("canvas");
-        ƒ.RenderManager.initialize();
-        ƒ.Debug.log(canvas);
-
-
-
-        let pong: ƒ.Node = createFudgeCraft();
-
-
-        let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+        
         cmpCamera.pivot.translateZ(50);
-
-        viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", pong, cmpCamera, canvas);
-        ƒ.Debug.log(viewport);
-
+    
+        let game: fudge.Node = createGame();
+        viewport = new fudge.Viewport();
+        viewport.initialize("Viewport", game, cmpCamera, canvas);
+        fudge.Debug.log(viewport);
 
         viewport.draw();
 
-        // FUDGE Core Game Loop and Starting the Loop
-        ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-        ƒ.Loop.start();
+        fudge.Loop.addEventListener(fudge.EVENT.LOOP_FRAME, update);
+        fudge.Loop.start();
     }
 
     function update(_event: Event): void {
-        // ƒ.Debug.log(keysPressed);
-        ƒ.RenderManager.update();
+
+        //cmpCamera.pivot.translateX(0.1);
+        //cmpCamera.pivot.rotateY(0.05);
+
+        fudge.RenderManager.update();
+        
         viewport.draw();
-
     }
 
-    function createFudgeCraft(): ƒ.Node {
+    function createGame(): fudge.Node {
 
-        let meshCube: ƒ.MeshCube = new ƒ.MeshCube();
-        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(1, 1, 1, 1)));
+        let game: fudge.Node = new fudge.Node("Game");
 
-        fudgeCraft.appendChild(createNode("WallLeft", meshCube, mtrSolidWhite, new ƒ.Vector2(-22, 0), new ƒ.Vector2(1, 30)));
-        fudgeCraft.appendChild(createNode("WallRight", meshCube, mtrSolidWhite, new ƒ.Vector2(22, 0), new ƒ.Vector2(1, 30)));
-        fudgeCraft.appendChild(createNode("WallTop", meshCube, mtrSolidWhite, new ƒ.Vector2(0, 15), new ƒ.Vector2(45, 1)));
-        fudgeCraft.appendChild(createNode("WallBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(0, -15), new ƒ.Vector2(45, 1)));
+        buildFragments(game);
 
-        fudgeCraft.appendChild(tFragment);
-        createTFragment(meshCube, mtrSolidWhite, tFragment);
+        return game;
+    }
 
-        fudgeCraft.appendChild(lFragment);
-        createLFragment(meshCube, mtrSolidWhite, lFragment);
+    function buildFragments(_game: fudge.Node): void {
+
+        let translationTemp: number = -20;
+
+        for (let i: number = 0; i < allFragmentsMatrices.length; i++) {
+
+            let baseBlock: fudge.Node = new fudge.Node("Base_Block_Fragment");
+            let mtrSoliColor: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 0, 0, 0)));
+            baseBlock.addComponent(new fudge.ComponentMesh(meshCube));
+            baseBlock.addComponent(new fudge.ComponentMaterial(mtrSoliColor));
+            baseBlock.addComponent(new fudge.ComponentTransform);
+            baseBlock.cmpTransform.local.translateX(translationTemp);
+
+            let buildingMtrx: boolean[] = allFragmentsMatrices[i];
+
+            for (let j: number = 0; j < buildingMtrx.length; j++) {
+                if (buildingMtrx[j] == true) {
+                    let subBlock: fudge.Node = new fudge.Node("subBlock");
+                    subBlock.addComponent(new fudge.ComponentMesh(meshCube));
+                    subBlock.addComponent(new fudge.ComponentMaterial(mtrSoliColor));
+                    subBlock.addComponent(new fudge.ComponentTransform);
+                    subBlock.cmpTransform.local.translateX(translationValues[j][0]);
+                    subBlock.cmpTransform.local.translateY(translationValues[j][1]);
+                    baseBlock.appendChild(subBlock);
+                    _game.appendChild(baseBlock);
+                }
+            }
+            translationTemp = translationTemp + 5; 
+        }
         
-        fudgeCraft.appendChild(zFragment);
-        createZFragment(meshCube, mtrSolidWhite, lFragment);
-
-        return fudgeCraft;
     }
 
+    function initializeBuildingMatrices(): void {
 
-    function createTFragment(meshCube: ƒ.MeshCube, mtrSolidWhite: ƒ.Material, tFragment: ƒ.Node): void {
-        tFragmentBottom = createNode("TFragmentBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(1, 1), new ƒ.Vector2(1, 1));
-        tFragmentTop = createNode("TFragmentTop", meshCube, mtrSolidWhite, new ƒ.Vector2(1, 2), new ƒ.Vector2(1, 1));
-        tFragmentLeft = createNode("TFragmentLeft", meshCube, mtrSolidWhite, new ƒ.Vector2(0, 2), new ƒ.Vector2(1, 1));
-        tFragmentRight = createNode("TFragmentRight", meshCube, mtrSolidWhite, new ƒ.Vector2(2, 2), new ƒ.Vector2(1, 1));
-        
-        tFragment.appendChild(tFragmentBottom);
-        tFragment.appendChild(tFragmentTop);
-        tFragment.appendChild(tFragmentLeft);
-        tFragment.appendChild(tFragmentRight);
+        let buildingMtrxIBlock: boolean[] = [false, true, false, 
+                                            false, false, false, 
+                                            false, true, false];
+        allFragmentsMatrices.push(buildingMtrxIBlock);    
+
+        let buildingMtrx2x2Block: boolean[] = [false, false, false,
+                                            false, false, true,
+                                            false, true, true];
+        allFragmentsMatrices.push(buildingMtrx2x2Block);
+
+        let buildingMtrxTBlock: boolean[] = [true, true, true, 
+                                            false, false, false, 
+                                            false, true, false];
+        allFragmentsMatrices.push(buildingMtrxTBlock);
+
+        let buildingMtrxLBlock: boolean[] = [false, true, false, 
+                                            false, false, false, 
+                                            false, true, true];
+        allFragmentsMatrices.push(buildingMtrxLBlock);
+
+        let buildingMtrxReversedLBlock: boolean[] = [false, true, false, 
+                                                    false, false, false, 
+                                                    true, true, false];
+        allFragmentsMatrices.push(buildingMtrxReversedLBlock);
+
+        let buildingMtrxZBlock: boolean[] = [true, true, false, 
+                                            false, false, false, 
+                                            false, true, true];
+        allFragmentsMatrices.push(buildingMtrxZBlock);
+
+        let buildingMtrxReversedZBlock: boolean[] = [false, true, true, 
+                                                    false, false, false, 
+                                                    true, true, false];
+        allFragmentsMatrices.push(buildingMtrxReversedZBlock);
+
     }
-
-    function createLFragment(meshCube: ƒ.MeshCube, mtrSolidWhite: ƒ.Material, lFragment: ƒ.Node): void {
-        lFragmentTop = createNode("lFragmentTop", meshCube, mtrSolidWhite, new ƒ.Vector2(10, 0), new ƒ.Vector2(1, 1));
-        lFragmentMiddle = createNode("lFragmentMiddle", meshCube, mtrSolidWhite, new ƒ.Vector2(10, -1), new ƒ.Vector2(1, 1));
-        lFragmentBottom = createNode("lFragmentBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(10, -2), new ƒ.Vector2(1, 1));
-        lFragmentBottomRight = createNode("lFragmentBottomRight", meshCube, mtrSolidWhite, new ƒ.Vector2(11, -2), new ƒ.Vector2(1, 1));
-
-        lFragment.appendChild(lFragmentTop);
-        lFragment.appendChild(lFragmentMiddle);
-        lFragment.appendChild(lFragmentBottom);
-        lFragment.appendChild(lFragmentBottomRight);
-    }
-
-    function createZFragment(meshCube: ƒ.MeshCube, mtrSolidWhite: ƒ.Material, zFragment: ƒ.Node): void {
-        zFragmentTopLeft = createNode("zFragmentTopLeft", meshCube, mtrSolidWhite, new ƒ.Vector2(-11, 1), new ƒ.Vector2(1, 1));
-        zFragmentTop = createNode("zFragmentTop", meshCube, mtrSolidWhite, new ƒ.Vector2(-10, 1), new ƒ.Vector2(1, 1));
-        zFragmentMiddle = createNode("zFragmentMiddle", meshCube, mtrSolidWhite, new ƒ.Vector2(-10, 0), new ƒ.Vector2(1, 1));
-        zFragmentBottom = createNode("zFragmentBottom", meshCube, mtrSolidWhite, new ƒ.Vector2(-10, -1), new ƒ.Vector2(1, 1));
-        zFragmentBottomRight = createNode("zFragmentBottomRight", meshCube, mtrSolidWhite, new ƒ.Vector2(-9, -1), new ƒ.Vector2(1, 1));
-
-        zFragment.appendChild(zFragmentTopLeft);
-        zFragment.appendChild(zFragmentTop);
-        zFragment.appendChild(zFragmentMiddle);
-        zFragment.appendChild(zFragmentBottom);
-        zFragment.appendChild(zFragmentBottomRight);
-    }
-
-    function createNode(_name: string, _mesh: ƒ.Mesh, _material: ƒ.Material, _translation: ƒ.Vector2, _scaling: ƒ.Vector2): ƒ.Node {
-
-        let node: ƒ.Node = new ƒ.Node(_name);
-        node.addComponent(new ƒ.ComponentTransform);
-        node.addComponent(new ƒ.ComponentMaterial(_material));
-        node.addComponent(new ƒ.ComponentMesh(_mesh));
-        node.cmpTransform.local.translate(_translation.toVector3());
-        node.getComponent(ƒ.ComponentMesh).pivot.scale(_scaling.toVector3());
-
-        return node;
-    }
+    
 
 }
